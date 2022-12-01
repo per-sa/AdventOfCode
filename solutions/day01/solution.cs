114bd70aba2d7ab55c55afb038fb3e0fd799efea
matheusclmb <ccolombomatheus@gmail.com>
2022-12-01 11:53:53 +0000

    day01 finished

---
+++ solutions/day01/solution.cs
@@ -0,0 +1,43 @@
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+namespace AdventOfCode.solutions.day01
+{
+
+    internal sealed class Solution
+    {
+
+        static void Main()
+        {
+
+            List<int> calories = new() { 0 };
+            using StreamReader reader = new(@"C:\Users\persa\Desktop\AdventOfCode\solutions\day01\input.txt", Encoding.UTF8);
+
+            while (!reader.EndOfStream)
+            {
+                string? line = reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    calories.Add(0);
+                    continue;
+                }
+
+                _ = int.TryParse(line, out int calory);
+                calories[^1] += calory;
+
+            }
+
+            // Part 1
+            Console.WriteLine(calories.Max());
+
+            // Part 2
+            Console.WriteLine(calories.OrderByDescending(x => x).Take(3).Sum());
+
+        }
+
+    }
+
+}
